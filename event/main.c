8bedf2a8dfd24ddb697b6fa71ca06ba59aec3160
Ben Andrew <ba405@cam.ac.uk>
2021-12-12 17:26:41 +0000

    Event timers added

--- event/main.c
+++ event/main.c
@@ -1,53 +1,46 @@
-// http://cvs.schmorp.de/libev/ev.pod
 
-#include <ev.h>
+#include <sys/types.h>
+#include <sys/timerfd.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
 
-#include <stdio.h> // for puts
 
-// every watcher type has its own typedef'd struct
-// with the name ev_TYPE
-ev_io stdin_watcher;
-ev_timer timeout_watcher;
+int main(int argc, char** argv) {
 
-// all watcher callbacks have a similar signature
-// this callback is called when data is readable on stdin
-static void
-stdin_cb (EV_P_ ev_io *w, int revents) {
-  puts ("stdin ready");
-  // for one-shot events, one must manually stop the watcher
-  // with its corresponding stop function.
-  ev_io_stop (EV_A_ w);
+  int timer = timerfd_create(CLOCK_REALTIME, 0);
 
-  // this causes all nested ev_run's to stop iterating
-  ev_break (EV_A_ EVBREAK_ALL);
-}
+  fd_set s;
+  FD_ZERO(&s);
 
-// another callback, this time for a time-out
-static void
-timeout_cb (EV_P_ ev_timer *w, int revents) {
-  puts ("timeout");
-  // this causes the innermost ev_run to stop iterating
-  ev_break (EV_A_ EVBREAK_ONE);
-}
+  struct itimerspec v = {
+    .it_value    = {1, 0},
+    .it_interval = {0, 0},
+  };
 
-int
-main (void) {
-  // use the default event loop unless you have special needs
-  struct ev_loop *loop = EV_DEFAULT;
+  timerfd_settime(timer, 0, &v, NULL);
+  FD_SET(timer, &s);
 
-  // initialise an io watcher, then start it
-  // this one will watch for stdin to become readable
-  ev_io_init (&stdin_watcher, stdin_cb, /*STDIN_FILENO*/ 0, EV_READ);
-  ev_io_start (loop, &stdin_watcher);
+  while (1) {
+    if (select(FD_SETSIZE, &s, NULL, NULL, NULL) < 0) {
+      exit(EXIT_FAILURE);
+    }
 
-  // initialise a timer watcher, then start it
-  // simple non-repeating 5.5 second timeout
-  ev_timer_init (&timeout_watcher, timeout_cb, 5.5, 0.);
-  ev_timer_start (loop, &timeout_watcher);
+    if(!FD_ISSET(timer, &s)) {
+      continue;
+    }
 
-  // now wait for events to arrive
-  ev_run (loop, 0);
+    timerfd_settime(timer, 0, &v, NULL);
 
-  // break was called, so exit
+    printf("FIRE\n");
+  }
+
+
+  close(timer);
+
   return 0;
 }
+
+
+
