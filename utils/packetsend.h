605d769b67590b63dad5207e82b7c06642a24805
Ben Andrew <ba405@cam.ac.uk>
2021-12-15 17:36:16 +0000

    ahhhhhhhhhhhh

--- utils/packetsend.h
+++ utils/packetsend.h
@@ -4,10 +4,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <net/route.h>
+
+#include "logging.h"
 
 #define MAXLINE 1024
+#define CONF_FILENAME "n1"
 
-char* read_conf_file(char* filename) {
+int get_node_id(void) {
+  return NULL;
+}
+
+char* read_node_conf_file(char* filename) {
   int read_size = -1;
   char* buf = (char*)malloc(sizeof(char) * MAXLINE);
   if (buf == NULL) {
@@ -29,26 +39,69 @@ char* read_conf_file(char* filename) {
   }
 }
 
-void set_ips(struct sockaddr_in* servaddr, struct sockaddr_in* cliaddr) {
+int get_neighbours(struct rtentry** entries) {
   /*
    * Parsing example:
    * interface eth0
    * ip address 10.0.0.2/24
    * !
    */
-  char* contents = read_conf_file(CONF_FILENAME);
+  char* contents = read_node_conf_file(CONF_FILENAME);
   char* pch = NULL;
   strtok(contents, "\n");
   strtok(NULL, " ");
   strtok(NULL, " ");
   pch = strtok(NULL, "/");
   log_f("pch: %s", pch);
-  if (strcmp(pch, "10.0.0.1") == 0) {
-    inet_pton(AF_INET, "10.0.0.2", &(servaddr->sin_addr));
-  } else {
-    inet_pton(AF_INET, "10.0.0.1", &(servaddr->sin_addr));
+
+  // Get number of neighbours
+  int n = 0;
+  char* start = contents;
+  pch = strsep(&contents, "\n");
+  while (pch != NULL) {
+    n++;
+    pch = strsep(&contents, "\n");
   }
-  inet_pton(AF_INET, pch, &(cliaddr->sin_addr));
+
+  *entries = (struct rtentry*) malloc(n * sizeof(struct rtentry));
+
+  contents = start;
+  int i = 0;
+  pch = strsep(&contents, "\n");
+  while(pch != NULL) {
+
+    struct rtentry route = (*entries)[i];
+    memset( &route, 0, sizeof( route ) );
+
+    struct sockaddr_in *addr = (struct sockaddr_in *)&route.rt_gateway;
+    addr->sin_family = AF_INET;
+    addr->sin_addr.s_addr = inet_addr( "10.0.2.2" );
+
+    addr = (struct sockaddr_in*) &route.rt_dst;
+    addr->sin_family = AF_INET;
+    addr->sin_addr.s_addr = INADDR_ANY;
+
+    addr = (struct sockaddr_in*) &route.rt_genmask;
+    addr->sin_family = AF_INET;
+    addr->sin_addr.s_addr = INADDR_ANY;
+
+    // TODO Add the interface name to the request
+    route.rt_flags = RTF_UP | RTF_GATEWAY;
+    route.rt_metric = 0;
+
+
+
+
+
+    pch = strsep(&contents, "\n");
+    i++;
+  }
+  
+
+
+
+  inet_pton(AF_INET, pch, &(destaddrs->sin_addr));
+  free(start);
 }
 
 #endif
