28ee67df0836a049933b8f9422d7683b992e3fb8
Ben Andrew <ba405@cam.ac.uk>
2022-01-02 21:17:22 +0000

    Heap tests pass fully

--- tests/algorithm/check_heap.c
+++ tests/algorithm/check_heap.c
@@ -18,39 +18,45 @@ START_TEST(test_heap_alloc) {
 END_TEST
 
 START_TEST(test_heap_build_heap) {
+	DijkstraNode* nodes = (DijkstraNode*)malloc(4 * sizeof(DijkstraNode));
 	MinHeap h = minheap_alloc(4);
 	h.size = 4;
 	for (int i = 0; i < 4; i++) {
-		h.nodes[i].node_id = i + 1;
-		h.nodes[i].dist = 4 - i;
+		nodes[i].id = i + 1;
+		nodes[i].tent_dist = 4 - i;
+		h.node_ptrs[i] = &nodes[i];
 	}
 	minheap_build_heap(&h);
 
-	kv v = minheap_pop(&h);
-	ck_assert_int_eq(v.node_id, 4);
-	ck_assert_int_eq(v.dist, 1);
+	DijkstraNode* v = minheap_pop(&h);
+	ck_assert_int_eq(v->id, 4);
+	ck_assert_int_eq(v->tent_dist, 1);
 	v = minheap_pop(&h);
-	ck_assert_int_eq(v.node_id, 3);
-	ck_assert_int_eq(v.dist, 2);
+	ck_assert_int_eq(v->id, 3);
+	ck_assert_int_eq(v->tent_dist, 2);
 	v = minheap_pop(&h);
-	ck_assert_int_eq(v.node_id, 2);
-	ck_assert_int_eq(v.dist, 3);
+	ck_assert_int_eq(v->id, 2);
+	ck_assert_int_eq(v->tent_dist, 3);
 	v = minheap_pop(&h);
-	ck_assert_int_eq(v.node_id, 1);
-	ck_assert_int_eq(v.dist, 4);
+	ck_assert_int_eq(v->id, 1);
+	ck_assert_int_eq(v->tent_dist, 4);
 
 	minheap_dealloc(&h);
+	free(nodes);
 }
 END_TEST
 
 START_TEST(test_heap_pop) {
 	MinHeap h = minheap_alloc(16);
-	
-	minheap_insert(&h, 1, 5);
+	DijkstraNode n;
+	n.id = 1;
+	n.tent_dist = 5;
+	minheap_insert(&h, &n);
 	ck_assert_int_eq(h.size, 1);
-	kv v = minheap_pop(&h);
-	ck_assert_int_eq(v.node_id, 1);
-	ck_assert_int_eq(v.dist, 5);
+	DijkstraNode* v = minheap_pop(&h);
+
+	ck_assert_int_eq(v->id, 1);
+	ck_assert_int_eq(v->tent_dist, 5);
 	ck_assert_int_eq(h.size, 0);
 
 	minheap_dealloc(&h);
@@ -59,25 +65,29 @@ END_TEST
 
 START_TEST(test_heap_insert) {
 	MinHeap h = minheap_alloc(4);
+	DijkstraNode nodes[3];
+	nodes[0].id = 1;
+	nodes[0].tent_dist = 10;
+	nodes[1].id = 2;
+	nodes[1].tent_dist = 0;
+	nodes[2].id = 3;
+	nodes[2].tent_dist = 5;
 
-	minheap_insert(&h, 1, 10);
+	minheap_insert(&h, &nodes[0]);
 	ck_assert_int_eq(h.size, 1);
-	minheap_insert(&h, 2, 0);
+	minheap_insert(&h, &nodes[1]);
 	ck_assert_int_eq(h.size, 2);
-	minheap_insert(&h, 3, 5);
+	minheap_insert(&h, &nodes[2]);
 	ck_assert_int_eq(h.size, 3);
-
-	kv v = minheap_pop(&h);
-	ck_assert_int_eq(v.node_id, 2);
-	ck_assert_int_eq(v.dist, 0);
-
+	DijkstraNode* v = minheap_pop(&h);
+	ck_assert_int_eq(v->id, 2);
+	ck_assert_int_eq(v->tent_dist, 0);
 	v = minheap_pop(&h);
-	ck_assert_int_eq(v.node_id, 3);
-	ck_assert_int_eq(v.dist, 5);
-
+	ck_assert_int_eq(v->id, 3);
+	ck_assert_int_eq(v->tent_dist, 5);
 	v = minheap_pop(&h);
-	ck_assert_int_eq(v.node_id, 1);
-	ck_assert_int_eq(v.dist, 10);
+	ck_assert_int_eq(v->id, 1);
+	ck_assert_int_eq(v->tent_dist, 10);
 
 	minheap_dealloc(&h);
 }
@@ -85,15 +95,24 @@ END_TEST
 
 START_TEST(test_heap_decrease_dist) {
 	MinHeap h = minheap_alloc(16);
-	minheap_insert(&h, 0, 5);
-	minheap_insert(&h, 1, 10);
-	minheap_insert(&h, 2, 20);
-	minheap_insert(&h, 3, 15);
+	DijkstraNode nodes[4];
+	nodes[0].id = 1;
+	nodes[0].tent_dist = 5;
+	nodes[1].id = 2;
+	nodes[1].tent_dist = 10;
+	nodes[2].id = 3;
+	nodes[2].tent_dist = 20;
+	nodes[2].id = 4;
+	nodes[2].tent_dist = 15;
+	minheap_insert(&h, &nodes[0]);
+	minheap_insert(&h, &nodes[1]);
+	minheap_insert(&h, &nodes[2]);
+	minheap_insert(&h, &nodes[3]);
 	minheap_decrease_dist(&h, 2, 0);
 
-	kv v = minheap_pop(&h);
-	ck_assert_int_eq(v.node_id, 2);
-	ck_assert_int_eq(v.dist, 0);
+	DijkstraNode* v = minheap_pop(&h);
+	ck_assert_int_eq(v->id, 2);
+	ck_assert_int_eq(v->tent_dist, 0);
 
 	minheap_dealloc(&h);
 }
