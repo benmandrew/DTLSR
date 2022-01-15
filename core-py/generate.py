80b6c298437c74033737cd8807f5842c3aad610a
Ben Andrew <ba405@cam.ac.uk>
2022-01-15 12:46:58 +0000

    Session generated dynamically

--- core-py/generate.py
+++ core-py/generate.py
@@ -13,28 +13,61 @@ from typing import List, Set, Tuple
 
 CONFIG_BASE = "/home/ben/projects/routing/configs"
 
-ip_prefixes: List[IpPrefixes] = []
+class Configuration:
+  def __init__(self, config: str, link_options: LinkOptions, session: Session) -> None:
+    self.session = session
+    self.ns: List[Node] = []
+    self.ip_prefixes: List[IpPrefixes] = []
 
-# create emulator instance for creating sessions and utility methods
-coreemu: CoreEmu = CoreEmu()
-session: Session = coreemu.create_session()
+    self._read_config(config)
+    self._create_links(link_options)
 
-session.service_manager.add(dtlsr.Heartbeat)
-session.service_manager.add(dtlsr.DTLSR)
+  def _read_config(self, config: str):
+    config_path = "{}/{}".format(CONFIG_BASE, config)
+    for file in os.listdir(config_path):
+      filename = os.fsdecode(file)
+      if filename[0] == "n" and not filename.endswith(".imn"):
+        full_path = "{}/{}".format(config_path, filename)
+        with open(full_path) as f:
+          rows = f.readlines()
+        rows = [row.rstrip() for row in rows]
+        self.ns.append(self._decode(filename, rows))
 
-session.set_state(EventTypes.CONFIGURATION_STATE)
+  def _create_links(self, options: LinkOptions):
+    seen_pairs: Set[Tuple[int, int]] = set()
+    for n in self.ns:
+      for neighbour_id in n.neighbour_ids:
+        if ((n.id, neighbour_id) not in seen_pairs
+            and (neighbour_id, n.id) not in seen_pairs):
+          seen_pairs.add((n.id, neighbour_id))
+    for i, (a, b) in enumerate(seen_pairs):
+      cn_a = get_node_with_id(self.ns, a)
+      cn_b = get_node_with_id(self.ns, b)
+      pref = IpPrefixes(
+        ip4_prefix="10.0.{}.0/24".format(i))
+      if_a = pref.create_iface(cn_a)
+      if_b = pref.create_iface(cn_b)
+      self.session.add_link(cn_a.id, cn_b.id, if_a, if_b, options)
+      self.ip_prefixes.append(pref)
 
-# configuring when creating a link
-options = LinkOptions(
-  bandwidth=54_000_000,
-  delay=1000,
-  dup=5,
-  loss=65.0,
-  jitter=0,
-)
+  def start_services(self):
+    for n in self.ns:
+      n.start_services(self.session)
+
+
+  def _decode(self, filename: str, rows: List[str]):
+    n = Node(filename[1:], self.session)
+    for row in rows:
+      source_ip, neighbour_ip, neighbour_id = self._decode_row(row)
+      n.add_neighbour(source_ip, neighbour_ip, neighbour_id)
+    return n
+
+  def _decode_row(self, row: str) -> Tuple[str, str, int]:
+    l = row.split(" - ")
+    return l[1], l[2], int(l[3])
 
 class Node:
-  def __init__(self, id: str) -> None:
+  def __init__(self, id: str, session: Session) -> None:
     self.id: int = int(id)
     self.source_ips: List[str] = []
     self.neighbour_ips: List[str] = []
@@ -48,56 +81,15 @@ class Node:
     self.source_ips.append(source_ip)
     self.neighbour_ips.append(neighbour_ip)
     self.neighbour_ids.append(neighbour_id)
+  
+  def start_services(self, session: Session):
+    session.services.create_service_files(self.core_node, dtlsr.Heartbeat)
+    session.services.create_service_files(self.core_node, dtlsr.DTLSR)
+    session.services.startup_service(self.core_node, dtlsr.Heartbeat)
+    session.services.startup_service(self.core_node, dtlsr.DTLSR)
 
-
-def get_node_with_id(ns: List[Node], id: int):
+def get_node_with_id(ns: List[Node], id: int) -> CoreNode:
   for n in ns:
     if n.core_node.id == id:
       return n.core_node
 
-def create_links(ns: List[Node]):
-  seen_pairs: Set[Tuple[int, int]] = set()
-  for n in ns:
-    for neighbour_id in n.neighbour_ids:
-      if ((n.id, neighbour_id) not in seen_pairs
-          and (neighbour_id, n.id) not in seen_pairs):
-        seen_pairs.add((n.id, neighbour_id))
-  for i, (a, b) in enumerate(seen_pairs):
-    pref = IpPrefixes(
-      ip4_prefix="10.0.{}.0/24".format(get_node_with_id(ns, i)))
-    iface1 = pref.create_iface()
-
-def decode(filename: str, rows: List[str]):
-  n = Node(filename[1:])
-  for row in rows:
-    source_ip, neighbour_ip, neighbour_id = decode_row(row)
-    n.add_neighbour(source_ip, neighbour_ip, neighbour_id)
-  return n
-
-def decode_row(row: str) -> Tuple[str, str, int]:
-  l = row.split(" - ")
-  return l[1], l[2], int(l[3])
-
-def main(config):
-  ns: List[Node] = []
-  config_path = "{}/{}".format(CONFIG_BASE, config)
-  for file in os.listdir(config_path):
-    filename = os.fsdecode(file)
-    if filename[0] == "n" and not filename.endswith(".imn"):
-      full_path = "{}/{}".format(config_path, filename)
-      with open(full_path) as f:
-        rows = f.readlines()
-      rows = [row.rstrip() for row in rows]
-      ns.append(decode(filename, rows))
-  create_links(ns)
-        
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-  main(sys.argv[1])
--- core-py/test.py
+++ core-py/test.py
@@ -8,34 +8,16 @@ from core.emulator.data import LinkOptions
 from core.nodes.base import CoreNode
 from core.services.coreservices import ServiceManager
 
-import dtlsr
+from generate import Configuration
 
-# ip nerator for example
-ip_pref_0 = IpPrefixes(ip4_prefix="10.0.0.0/24")
-ip_pref_1 = IpPrefixes(ip4_prefix="10.0.1.0/24")
+import dtlsr
 
 # create emulator instance for creating sessions and utility methods
 coreemu: CoreEmu = CoreEmu()
 session: Session = coreemu.create_session()
-
 session.service_manager.add(dtlsr.Heartbeat)
 session.service_manager.add(dtlsr.DTLSR)
-
 session.set_state(EventTypes.CONFIGURATION_STATE)
-
-# create nodes
-options = NodeOptions(name="n1", x=100, y=100)
-n1: CoreNode = session.add_node(CoreNode, options=options)
-session.services.set_service(n1.id, "DTLSR")
-session.services.set_service(n1.id, "Heartbeat")
-options = NodeOptions(name="n2", x=300, y=100)
-n2: CoreNode = session.add_node(CoreNode, options=options)
-session.services.set_service(n2.id, "DTLSR")
-session.services.set_service(n2.id, "Heartbeat")
-options = NodeOptions(name="n3", x=500, y=100)
-n3: CoreNode = session.add_node(CoreNode, options=options)
-session.services.set_service(n3.id, "DTLSR")
-session.services.set_service(n3.id, "Heartbeat")
 
 # configuring when creating a link
 options = LinkOptions(
@@ -46,32 +28,10 @@ options = LinkOptions(
   jitter=0,
 )
 
-# link nodes together
-iface1_1 = ip_pref_0.create_iface(n1)
-iface2_1 = ip_pref_0.create_iface(n2)
-iface2_2 = ip_pref_1.create_iface(n2)
-iface3_1 = ip_pref_1.create_iface(n3)
-print(iface1_1.ip4)
-print(iface2_1.ip4)
-print(iface2_2.ip4)
-print(iface3_1.ip4)
-session.add_link(n1.id, n2.id, iface1_1, iface2_1, options)
-session.add_link(n2.id, n3.id, iface2_2, iface3_1, options)
 
-session.instantiate()
+conf = Configuration("headless", options, session)
 
-session.services.create_service_files(n1, dtlsr.Heartbeat)
-session.services.create_service_files(n2, dtlsr.Heartbeat)
-session.services.create_service_files(n3, dtlsr.Heartbeat)
-session.services.create_service_files(n1, dtlsr.DTLSR)
-session.services.create_service_files(n2, dtlsr.DTLSR)
-session.services.create_service_files(n3, dtlsr.DTLSR)
-session.services.startup_service(n1, dtlsr.Heartbeat)
-session.services.startup_service(n2, dtlsr.Heartbeat)
-session.services.startup_service(n3, dtlsr.Heartbeat)
-session.services.startup_service(n1, dtlsr.DTLSR)
-session.services.startup_service(n2, dtlsr.DTLSR)
-session.services.startup_service(n3, dtlsr.DTLSR)
+conf.start_services()
 
 # print("Heartbeat valid:", session.services.validate_service(n1, dtlsr.Heartbeat))
 # print("DTLSR valid:", session.services.validate_service(n2, dtlsr.DTLSR))
